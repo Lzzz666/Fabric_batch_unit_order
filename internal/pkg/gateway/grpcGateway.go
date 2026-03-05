79624a1f821c807bc2cf8e220792ca38af717cf8
Lzzz666 <zhenyuliu0825@gmail.com>
2026-03-05 16:24:20 +0800

    gcp setup

--- internal/pkg/gateway/grpcGateway.go
+++ internal/pkg/gateway/grpcGateway.go
@@ -14,8 +14,8 @@ import (
 func (gs *Server) connectGRPC() error {
 	address := gs.options.SequencerAddress
 	if address == "" {
-		address = "172.20.10.5:7073" // 默認 gRPC 端口（不同於 UDP 的 7072）
-		// address = "10.140.0.10:7073"
+		// address = "172.20.10.5:7073" // 默認 gRPC 端口（不同於 UDP 的 7072）
+		address = "10.140.0.10:7073"
 	}
 
 	fmt.Printf("🔌 [gRPC Gateway] 正在連接到 sequencer: %s\n", address)
